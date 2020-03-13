bc8fef312fe62cc0c2deca5c1c8565e90da90b75
unknown <osalimohammadamin@gmail.com>
2020-03-13 14:57:49 +0330

    Added postgresql

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
